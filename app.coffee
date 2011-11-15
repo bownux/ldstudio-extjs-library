f47576197cb8fe0f46decab97a3b79a1b556814c
Larry Rivera <larry@ldstudio.com>
2011-11-14 17:10:13 -0800

    Port Config
    Update port config for heroku

--- app.coffee
+++ app.coffee
@@ -38,6 +38,8 @@ app.get '/widget', routes.widget
 
 ## Only listen on $ node app.js
 
+port = process.env.PORT or 3000
+
 if !module.parent
-        app.listen 4000
+        app.listen port
         console.log "Express server listening on port %d in %s mode", app.address().port, app.settings.env
--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 (function() {
-
   /*
   Module dependencies.
   */
-
-  var app, express, routes;
-
+  var app, express, port, routes;
   express = require('express');
-
   routes = require('./config/routes.js');
-
   app = module.exports = express.createServer();
-
   app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -30,25 +24,20 @@
     app.use(app.router);
     return app.use(express.static(__dirname + '/public'));
   });
-
   app.configure('development', function() {
     return app.use(express.errorHandler({
       dumpExceptions: true,
       showStack: true
     }));
   });
-
   app.configure('production', function() {
     return app.use(express.errorHandler);
   });
-
   app.get('/', routes.index);
-
   app.get('/widget', routes.widget);
-
+  port = process.env.PORT || 3000;
   if (!module.parent) {
-    app.listen(4000);
+    app.listen(port);
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   }
-
 }).call(this);
